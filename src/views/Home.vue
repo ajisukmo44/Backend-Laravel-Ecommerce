5682cd024f18876f2a53e9c8cec02e6dda86ec0a
ajisukmo44 <ajisuk31@gmail.com>
2020-03-07 11:29:36 +0700

    mengupdate file

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <OwlCarousel />
+    <ItemsCarousel />
+    <InstaGallery />
+    <LogoPatner />
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+// // @ is an alias to /src
+import Header from "@/components/Header.vue";
+import OwlCarousel from "@/components/OwlCarousel.vue";
+import ItemsCarousel from "@/components/ItemsCarousel.vue";
+import InstaGallery from "@/components/InstaGallery.vue";
+import LogoPatner from "@/components/LogoPatner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    OwlCarousel,
+    ItemsCarousel,
+    InstaGallery,
+    LogoPatner,
+    Footer
+  }
+};
+</script>
